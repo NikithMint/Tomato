2a4fafe443741f57e431895e5ce35bad138422c7
Nikith Tankashala <108892614+NikithMint@users.noreply.github.com>
2024-05-04 23:34:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
+import Navbar from './Navbar/Navbar'
 import './index.css'
 import { Routes,Route } from 'react-router-dom'
 import Home from './Home/Home'
